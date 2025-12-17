bb568c13fb9cfbee559a46e06d461cac8c676e65
daleelerp <daleel.erp.site@gmail.com>
2025-12-17 17:46:24 +0200

    Revert "Add redirect from daleel.site to www.daleel.site"
    This reverts commit 425568b0a97b42b17674049e66a0e0a8c3f35c83.

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        has: [
-          {
-            type: "host",
-            value: "daleel.site",
-          },
-        ],
-        destination: "https://www.daleel.site",
-        permanent: true,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
